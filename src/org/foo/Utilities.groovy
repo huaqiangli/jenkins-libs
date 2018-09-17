e2b9441f48a1fbb83843e8a3665f3d74a1d1f8d0
lihuaqiang <lihuaqiang@le.com>
2018-09-17 12:49:55 +0800

    fix name

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -2,7 +2,7 @@ package org.foo
 class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
-  def mvni3(args) {
+  def mvn3(args) {
     steps.sh "${steps.tool 'M3'}/bin/mvn -o ${args}"
   }
 }
