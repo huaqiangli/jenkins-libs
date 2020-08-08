cbdff951a44b45a2974653d1c2ee9873acd3e5bb
Li huaqiang <huaqiangli@hotmail.com>
2020-08-08 15:55:36 +0800

    Create helloWorldPipelineDSL.groovy

---
+++ vars/helloWorldPipelineDSL.groovy
@@ -0,0 +1,17 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+ 
+    pipeline {
+        agent any
+        stages {
+            stage('hello') {
+                steps {
+                    helloWorld(name: "${pipelineParams.name}", dayOfWeek: "${pipelineParams.dayOfWeek}")
+                }
+            }
+        }
+    }
+}
